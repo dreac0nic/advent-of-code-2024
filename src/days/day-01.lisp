0df6155074e29fb37558b80a9b7dc51b21e96d3c
SpenSer Bray <spenser.bray@a-dec.com>
2024-12-07 04:45:27 -0800

    Riff on day on as an example

--- src/days/day-01.lisp
+++ src/days/day-01.lisp
@@ -1,3 +1,13 @@
 ;;;; days/day-01.lisp
 
 (in-package #:days.day-01)
+
+;; (with-puzzle
+;;     (:day 1
+;;      :year 2024)
+;;   (:input-binding puzzle-input)
+;;   (->> (split "\\n" puzzle-input)
+;;        (mapcar (lambda (line)
+;;                  (->> (split "   " line)
+;;                       (mapcar #'parse-integer)
+;;                       (apply #'cons))))))
