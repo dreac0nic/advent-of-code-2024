8b6b12a15248cd7b95ea478e93fb1762d4bef0f6
SpenSer Bray <spenser.bray@a-dec.com>
2024-12-14 15:13:50 -0800

    Export all functions on utiltiy

--- src/package.lisp
+++ src/package.lisp
@@ -1,7 +1,8 @@
 ;;;; package.lisp
 
 (defpackage #:utility
-  (:use #:cl #:arrows))
+  (:use #:cl #:arrows)
+  (:export :single? :append1 :map-int :filter :most :compose :disjoin :conjoin :curry :rcurry :always :clamp :juxt))
 
 (defpackage #:days.day-01
   (:use #:cl #:arrows #:cl-ppcre #:utility #:cl-advent-of-code))
